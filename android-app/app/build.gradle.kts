1c5df73e0615e30a34e32e6addf5bda7d15eb1c1
HanChen <602506320@qq.com>
2026-05-18 23:00:53 +0800

    fix(android): prevent Activity recreation on fullscreen orientation change

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.vipsearch.app"
     minSdk = 26
     targetSdk = 35
-    versionCode = 6
-    versionName = "0.2.5"
+    versionCode = 7
+    versionName = "0.2.6"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
